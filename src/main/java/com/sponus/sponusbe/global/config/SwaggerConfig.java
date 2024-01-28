675acf4fb88d3988d693953af93a57b266802852
marooo326 <ktnqkr01@gmail.com>
2024-01-28 18:06:03 +0900

    fix: swagger 토큰 삽입 문제 수정

--- src/main/java/com/sponus/sponusbe/global/config/SwaggerConfig.java
+++ src/main/java/com/sponus/sponusbe/global/config/SwaggerConfig.java
@@ -7,19 +7,23 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class SwaggerConfig {
 	// url : http://localhost:8080/swagger-ui/index.html#/
+	private static final String SECURITY_SCHEME_NAME = "bearerAuth";
+
 	@Bean
-	public OpenAPI getOpenApi() {
+	public OpenAPI api() {
 		Server server = new Server().url("/");
 
 		return new OpenAPI()
-			.info(getSwaggerInfo())
+			.addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
 			.components(authSetting())
+			.info(getSwaggerInfo())
 			.addServersItem(server);
 	}
 
@@ -35,23 +39,31 @@ public class SwaggerConfig {
 	}
 
 	private Components authSetting() {
+
 		return new Components()
-			.addSecuritySchemes(
-				"access-token",
+			.addSecuritySchemes(SECURITY_SCHEME_NAME,
 				new SecurityScheme()
+					.name(SECURITY_SCHEME_NAME)
 					.type(SecurityScheme.Type.HTTP)
 					.scheme("bearer")
-					.bearerFormat("JWT")
-					.in(SecurityScheme.In.HEADER)
-					.name("Authorization"))
-			.addSecuritySchemes(
-				"refresh-token",
-				new SecurityScheme()
-					.type(SecurityScheme.Type.HTTP)
-					.scheme("bearer")
-					.bearerFormat("JWT")
-					.in(SecurityScheme.In.HEADER)
-					.name("refreshToken"));
+					.bearerFormat("JWT"));
+		// return new Components()
+		// 	.addSecuritySchemes(
+		// 		"access-token",
+		// 		new SecurityScheme()
+		// 			.type(SecurityScheme.Type.HTTP)
+		// 			.scheme("Bearer")
+		// 			.bearerFormat("JWT")
+		// 			.in(SecurityScheme.In.HEADER)
+		// 			.name("Authorization"))
+		// 	.addSecuritySchemes(
+		// 		"refresh-token",
+		// 		new SecurityScheme()
+		// 			.type(SecurityScheme.Type.HTTP)
+		// 			.scheme("Bearer")
+		// 			.bearerFormat("JWT")
+		// 			.in(SecurityScheme.In.HEADER)
+		// 			.name("refreshToken"));
 	}
 }
 
