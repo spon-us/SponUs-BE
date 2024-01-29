e86b0c0850a8797e99dc123032d399715eeade20
Yuje-Lee <65337423+dbwp031@users.noreply.github.com>
2024-01-29 21:40:06 +0900

    ✨ feat: 내 조직 조회 / 수정 / 삭제  기능 개발 (#55)
    * :sparkles: feat: Organization 관련 Response 생성
    
    * :sparkles: feat: Organization 조회 기능 구현
    
    * :sparkles: feat: 내 조직 수정 기능 구현
    
    * :white_check_mark: test: 내 조직 조회 & 업데이트 테스트 request 추가
    
    * :recycle: refactor: 조직 수정 매서드 입력 파라미터 organization에서 organizationId로 수정
    
    Organization을 외부에서 찾은 후 전달해야 하는 불필요한 로직 제거를 위함
    
    * :sparkles: feat: 내 조직 삭제(soft delete, 비활성화) 기능 구현
    
    * :white_check_mark: test: 내 조직 삭제(soft delete) 기능 테스트 request 구현
    
    ---------
    
    Co-authored-by: Yuje Lee <yj.lee@promptech.co.kr>

---
+++ src/main/java/com/sponus/sponusbe/domain/organization/dto/OrganizationDetailGetResponse.java
@@ -0,0 +1,56 @@
+package com.sponus.sponusbe.domain.organization.dto;
+
+import com.sponus.sponusbe.domain.organization.entity.Organization;
+import com.sponus.sponusbe.domain.organization.entity.enums.OrganizationStatus;
+import com.sponus.sponusbe.domain.organization.entity.enums.OrganizationType;
+import com.sponus.sponusbe.domain.organization.entity.enums.SuborganizationType;
+
+import java.util.List;
+
+public record OrganizationDetailGetResponse(
+        Long organizationId,
+        String name,
+        String email,
+        String password,
+        String location,
+        String description,
+        String imageUrl,
+        OrganizationType organizationType,
+        SuborganizationType suborganizationType,
+        String managerName,
+        String managerPosition,
+        String managerEmail,
+        String managerPhone,
+        String managerAvailableDay,
+        String managerAvailableHour,
+        String managerContactPreference,
+        OrganizationStatus organizationStatus,
+        List<TagGetResponse> tags,
+        List<OrganizationLinkGetResponse> links
+) {
+    public static OrganizationDetailGetResponse from(Organization organization) {
+        List<TagGetResponse> tagGetResponses = TagGetResponse.getTagResponses(organization);
+        List<OrganizationLinkGetResponse> linkGetResponses = OrganizationLinkGetResponse.getOrganizationLinkResponses(organization);
+        return new OrganizationDetailGetResponse(
+                organization.getId(),
+                organization.getName(),
+                organization.getEmail(),
+                organization.getPassword(),
+                organization.getLocation(),
+                organization.getDescription(),
+                organization.getImageUrl(),
+                organization.getOrganizationType(),
+                organization.getSuborganizationType(),
+                organization.getManagerName(),
+                organization.getManagerPosition(),
+                organization.getManagerEmail(),
+                organization.getManagerPhone(),
+                organization.getManagerAvailableDay(),
+                organization.getManagerAvailableHour(),
+                organization.getManagerContactPreference(),
+                organization.getOrganizationStatus(),
+                tagGetResponses,
+                linkGetResponses
+        );
+    }
+}
