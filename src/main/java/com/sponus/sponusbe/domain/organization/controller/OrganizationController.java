cbd300fd2e9ce045bb015047b5c32e9164bac499
Yuje-Lee <65337423+dbwp031@users.noreply.github.com>
2024-01-30 21:25:57 +0900

    ✨ feat: 조직 조회 기능 개발 (#65)
    * :sparkles: feat: 조직 조회 기능 개발
    
    * :white_check_mark: test: 조직 조회 request 구현

--- src/main/java/com/sponus/sponusbe/domain/organization/controller/OrganizationController.java
+++ src/main/java/com/sponus/sponusbe/domain/organization/controller/OrganizationController.java
@@ -3,6 +3,7 @@ package com.sponus.sponusbe.domain.organization.controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ import com.sponus.sponusbe.domain.organization.dto.OrganizationJoinRequest;
 import com.sponus.sponusbe.domain.organization.dto.OrganizationJoinResponse;
 import com.sponus.sponusbe.domain.organization.dto.OrganizationUpdateRequest;
 import com.sponus.sponusbe.domain.organization.entity.Organization;
+import com.sponus.sponusbe.domain.organization.service.OrganizationQueryService;
 import com.sponus.sponusbe.domain.organization.service.OrganizationService;
 import com.sponus.sponusbe.global.common.ApiResponse;
 
@@ -26,6 +28,7 @@ import lombok.RequiredArgsConstructor;
 public class OrganizationController {
 
 	private final OrganizationService organizationService;
+	private final OrganizationQueryService organizationQueryService;
 
 	@PostMapping("/join")
 	public ApiResponse<OrganizationJoinResponse> join(@Valid @RequestBody OrganizationJoinRequest request) {
@@ -57,5 +60,10 @@ public class OrganizationController {
 	public ApiResponse<Void> deleteMyOrganization(@AuthOrganization Organization organization) {
 		organizationService.deactivateOrganization(organization.getId());
 		return ApiResponse.onSuccess(null);
+	}
+
+	@GetMapping("/{organizationId}")
+	public ApiResponse<OrganizationDetailGetResponse> getOrganization(@PathVariable Long organizationId) {
+		return ApiResponse.onSuccess(organizationQueryService.getOrganization(organizationId));
 	}
 }
