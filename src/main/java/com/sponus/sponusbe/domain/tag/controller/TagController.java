2539d80c52c78148f19079aa9dc4a82d9eea17f6
Yuje Lee <dbwp031@gmail.com>
2024-02-03 18:08:02 +0900

    :sparkles: feat: tag 조회 기능 개발

--- src/main/java/com/sponus/sponusbe/domain/tag/controller/TagController.java
+++ src/main/java/com/sponus/sponusbe/domain/tag/controller/TagController.java
@@ -1,5 +1,7 @@
 package com.sponus.sponusbe.domain.tag.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,8 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.sponus.sponusbe.auth.annotation.AuthOrganization;
 import com.sponus.sponusbe.domain.organization.entity.Organization;
+import com.sponus.sponusbe.domain.tag.dto.TagGetResponse;
 import com.sponus.sponusbe.domain.tag.dto.request.TagCreateRequest;
 import com.sponus.sponusbe.domain.tag.dto.resposne.TagCreateResponse;
+import com.sponus.sponusbe.domain.tag.service.TagQueryService;
 import com.sponus.sponusbe.domain.tag.service.TagService;
 import com.sponus.sponusbe.global.common.ApiResponse;
 
@@ -21,10 +25,16 @@ import lombok.extern.slf4j.Slf4j;
 @RestController
 public class TagController {
 	private final TagService tagService;
+	private final TagQueryService tagQueryService;
 
 	@PostMapping
 	public ApiResponse<TagCreateResponse> createTag(@AuthOrganization Organization organization,
 		@RequestBody TagCreateRequest request) {
 		return ApiResponse.onSuccess(tagService.createTag(organization.getId(), request));
+	}
+
+	@GetMapping("/{tagId}")
+	public ApiResponse<TagGetResponse> getTag(@PathVariable Long tagId) {
+		return ApiResponse.onSuccess(tagQueryService.getTag(tagId));
 	}
 }
