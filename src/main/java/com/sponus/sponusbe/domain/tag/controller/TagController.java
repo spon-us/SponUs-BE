812651d5455a7f29cb03eb5fce27276bbd6ca422
Yuje-Lee <65337423+dbwp031@users.noreply.github.com>
2024-02-03 17:49:33 +0900

    ✨ feat: 태그 생성 기능 개발 (#89)
    * :truck: rename: Tag 도메인 분리
    
    기존 Organization에서 다루던 Tag 엔티티
    개발 편의를 위해 도메인 분리, 그에 따른 파일 이동
    
    * :sparkles: feat: Tag 도메인 기본 클래스 생성
    
    * :truck: rename: Tag 도메인 생성에 따른 TagResponse 클래스 도메인 패키지 내로 이동
    
    * :sparkles: feat: OrganizationTag 삭제에 따른 연관관계 수정
    
    * :sparkles: feat: Tag 생성 기능 개발
    
    * :white_check_mark: test: Tag 생성 기능 테스트 request 구현

---
+++ src/main/java/com/sponus/sponusbe/domain/tag/controller/TagController.java
@@ -0,0 +1,30 @@
+package com.sponus.sponusbe.domain.tag.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sponus.sponusbe.auth.annotation.AuthOrganization;
+import com.sponus.sponusbe.domain.organization.entity.Organization;
+import com.sponus.sponusbe.domain.tag.dto.request.TagCreateRequest;
+import com.sponus.sponusbe.domain.tag.dto.resposne.TagCreateResponse;
+import com.sponus.sponusbe.domain.tag.service.TagService;
+import com.sponus.sponusbe.global.common.ApiResponse;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/tags")
+@RestController
+public class TagController {
+	private final TagService tagService;
+
+	@PostMapping
+	public ApiResponse<TagCreateResponse> createTag(@AuthOrganization Organization organization,
+		@RequestBody TagCreateRequest request) {
+		return ApiResponse.onSuccess(tagService.createTag(organization.getId(), request));
+	}
+}
