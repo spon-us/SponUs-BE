65df7c52ffb2a4f1c757e120ca9198dc702dc03f
Kim Dae Hwi <121790935+marooo326@users.noreply.github.com>
2024-02-04 16:35:46 +0900

    ✨ feat: 제안 조회시 첨부파일 추가, 공고 상태 변경 (#121)
    * refactor: 공고 상태 변경 (OPENED, CLOSED)
    
    * feat: 제안서 작성시 첨부파일 추가

---
+++ src/main/java/com/sponus/sponusbe/domain/propose/repository/ProposeAttachmentRepository.java
@@ -0,0 +1,9 @@
+package com.sponus.sponusbe.domain.propose.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sponus.sponusbe.domain.propose.entity.ProposeAttachment;
+
+public interface ProposeAttachmentRepository extends JpaRepository<ProposeAttachment, Long> {
+
+}
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -38,7 +38,7 @@ VALUES ('무신사 스폰서십',
         'SPONSORSHIP',
         'MARKETING',
         '무신사 스폰서십을 진행할 대학교 학생회를 모집합니다.',
-        'POSTED',
+        'IN_PROGRESS',
         0, 1);
 
 INSERT INTO tag (organization_id, tag_name)
