460beae1112fb2649d5e4761351c6273451b40b4
marooo326 <ktnqkr01@gmail.com>
2024-02-15 18:49:05 +0900

    feat: 제안 정보를 날짜 기준으로 그룹핑 및 내림차순 반환하도록 변경

--- src/main/java/com/sponus/sponusbe/domain/propose/controller/ProposeController.java
+++ src/main/java/com/sponus/sponusbe/domain/propose/controller/ProposeController.java
@@ -1,6 +1,5 @@
 package com.sponus.sponusbe.domain.propose.controller;
 
-import java.io.IOException;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -20,9 +19,9 @@ import com.sponus.sponusbe.domain.organization.entity.Organization;
 import com.sponus.sponusbe.domain.propose.dto.request.ProposeCreateRequest;
 import com.sponus.sponusbe.domain.propose.dto.request.ProposeStatusUpdateRequest;
 import com.sponus.sponusbe.domain.propose.dto.request.ProposeUpdateRequest;
+import com.sponus.sponusbe.domain.propose.dto.response.DateGroupedProposeResponse;
 import com.sponus.sponusbe.domain.propose.dto.response.ProposeCreateResponse;
 import com.sponus.sponusbe.domain.propose.dto.response.ProposeDetailGetResponse;
-import com.sponus.sponusbe.domain.propose.dto.response.ProposeSummaryGetResponse;
 import com.sponus.sponusbe.domain.propose.service.ProposeQueryService;
 import com.sponus.sponusbe.domain.propose.service.ProposeService;
 import com.sponus.sponusbe.global.common.ApiResponse;
@@ -45,7 +44,7 @@ public class ProposeController {
 		@AuthOrganization Organization authOrganization,
 		@RequestPart("request") @Valid ProposeCreateRequest request,
 		@RequestPart(value = "attachments", required = false) List<MultipartFile> attachments
-	) throws IOException {
+	) {
 		return ApiResponse.onSuccess(
 			proposeService.createPropose(
 				authOrganization,
@@ -56,14 +55,14 @@ public class ProposeController {
 	}
 
 	@GetMapping("/sent")
-	public ApiResponse<List<ProposeSummaryGetResponse>> getSentProposes(
+	public ApiResponse<List<DateGroupedProposeResponse>> getSentProposes(
 		@AuthOrganization Organization authOrganization
 	) {
 		return ApiResponse.onSuccess(proposeQueryService.getSentProposes(authOrganization));
 	}
 
 	@GetMapping("/received")
-	public ApiResponse<List<ProposeSummaryGetResponse>> getReceivedProposes(
+	public ApiResponse<List<DateGroupedProposeResponse>> getReceivedProposes(
 		@AuthOrganization Organization authOrganization,
 		@RequestParam Long announcementId
 	) {
