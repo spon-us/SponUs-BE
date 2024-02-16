dcc5a61221d54f8b3332276987a30c15f84fdd43
Kim Dae Hwi <121790935+marooo326@users.noreply.github.com>
2024-02-16 09:44:24 +0900

    ✨ feat: 제안 결제 성공 시 상태 변경 (#208)
    * feat: 제안 결제 성공 시 상태 변경
    
    * feat: 요청 & 응답 형식 변경

--- src/main/java/com/sponus/sponusbe/domain/payment/controller/PaymentController.java
+++ src/main/java/com/sponus/sponusbe/domain/payment/controller/PaymentController.java
@@ -1,19 +1,15 @@
 package com.sponus.sponusbe.domain.payment.controller;
 
-import java.io.IOException;
-
-import javax.annotation.PostConstruct;
-
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.siot.IamportRestClient.IamportClient;
-import com.siot.IamportRestClient.exception.IamportResponseException;
-import com.siot.IamportRestClient.response.IamportResponse;
 import com.siot.IamportRestClient.response.Payment;
+import com.sponus.sponusbe.auth.annotation.AuthOrganization;
+import com.sponus.sponusbe.domain.organization.entity.Organization;
 import com.sponus.sponusbe.domain.payment.dto.PaymentRequest;
+import com.sponus.sponusbe.domain.payment.service.PaymentService;
+import com.sponus.sponusbe.global.common.ApiResponse;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -23,24 +19,12 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class PaymentController {
 
-	@Value("${PORT_ONE_KEY}")
-	private String restApiKey;
-	@Value("${PORT_ONE_SECRET}")
-	private String restApiSecret;
-
-	private IamportClient iamportClient;
-
-	@PostConstruct
-	public void init() {
-		this.iamportClient = new IamportClient(restApiKey, restApiSecret);
-	}
+	private final PaymentService paymentService;
 
 	@PostMapping("/api/v1/payments")
-	public IamportResponse<Payment> paymentByImpUid(
-		@RequestBody PaymentRequest request) throws
-		IamportResponseException,
-		IOException {
-		log.info("Payment Request : {}", request.toString());
-		return iamportClient.paymentByImpUid(request.impUid());
+	public ApiResponse<Payment> paymentByImpUid(
+		@AuthOrganization Organization authOrganization,
+		@RequestBody PaymentRequest request) {
+		return ApiResponse.onSuccess(paymentService.paymentByImpUid(request, authOrganization));
 	}
 }
