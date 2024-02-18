77ad828db261c2c1ef7d6fa290bb79b820fd5ba9
marooo326 <ktnqkr01@gmail.com>
2024-02-18 13:24:35 +0900

    feat: 제안 완료(협업 완료) 상태 추가

--- src/main/java/com/sponus/sponusbe/domain/propose/entity/ProposeStatus.java
+++ src/main/java/com/sponus/sponusbe/domain/propose/entity/ProposeStatus.java
@@ -6,7 +6,7 @@ import com.sponus.sponusbe.domain.propose.exception.ProposeException;
 public enum ProposeStatus {
 	// 제안을 보낸 측: PENDING
 	// 제안을 받은 측: VIEWED, ACCEPTED, REJECTED, SUSPENDED, PAID
-	PENDING, VIEWED, ACCEPTED, REJECTED, SUSPENDED, PAID;
+	PENDING, VIEWED, ACCEPTED, REJECTED, SUSPENDED, PAID, COMPLETED;
 
 	public static ProposeStatus of(String input) {
 		try {
