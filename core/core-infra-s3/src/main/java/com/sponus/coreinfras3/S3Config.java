48ab05865418f69936398b09033b48884e7b230b
seheonnn <ho78901@naver.com>
2024-03-12 19:03:01 +0900

    ️♻️ refactor: S3 모듈 분리 (#250)
    * ♻️refactor: s3 모듈 분리
    
    * ♻️refactor: s3 모듈 분리
    
    * 🐛 fix: 로컬 빌드 에러 해결
    
    * 🐛 fix: application.yml 변경
    
    * ♻️ refactor: 불필요한 주석 삭제
    
    * ♻️ refactor: 불필요한 의존성 삭제
    
    * 🐛 fix: db.yml local profile 주석처리

--- core/core-infra-s3/src/main/java/com/sponus/coreinfras3/S3Config.java
+++ core/core-infra-s3/src/main/java/com/sponus/coreinfras3/S3Config.java
@@ -1,4 +1,4 @@
-package com.sponus.sponusbe.domain.s3;
+package com.sponus.coreinfras3;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
