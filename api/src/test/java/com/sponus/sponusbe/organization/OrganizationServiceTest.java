aee7a9d7fc99be1bffbdb0a886ab516eaf2da6c3
seheonnn <ho78901@naver.com>
2024-04-09 01:22:15 +0900

    🔨 test: 테스트 코드 변경

--- api/src/test/java/com/sponus/sponusbe/organization/OrganizationServiceTest.java
+++ api/src/test/java/com/sponus/sponusbe/organization/OrganizationServiceTest.java
@@ -8,14 +8,11 @@ import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.sponus.coredomain.domain.organization.enums.OrganizationType;
 import com.sponus.sponusbe.domain.organization.dto.OrganizationJoinRequest;
@@ -24,18 +21,16 @@ import com.sponus.sponusbe.domain.organization.service.OrganizationService;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@Transactional
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class OrganizationServiceTest {
 
 	@Autowired
 	private MockMvc mockMvc;
 
-	@SpyBean
+	@Autowired
 	private OrganizationService organizationService;
 
 	@Order(1)
-	@Rollback(value = false)
 	@Test
 	void joinTest() {
 
