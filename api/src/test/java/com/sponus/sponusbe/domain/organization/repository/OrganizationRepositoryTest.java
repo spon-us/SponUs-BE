fdd51d8578b9cef47f28788e537fd4dba4a83bc4
seheonnn <ho78901@naver.com>
2024-04-11 17:27:11 +0900

    🔨 test: 테스트 코드 수정

--- api/src/test/java/com/sponus/sponusbe/domain/organization/repository/OrganizationRepositoryTest.java
+++ api/src/test/java/com/sponus/sponusbe/domain/organization/repository/OrganizationRepositoryTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.sponus.coredomain.domain.organization.Organization;
