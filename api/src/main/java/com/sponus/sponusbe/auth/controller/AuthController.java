5f4c69d44d3610218961e434104d462bf273d754
JungTae Kwon <oxdjww@gmail.com>
2024-05-28 23:33:16 +0900

    refactor: Refactor AuthController (#294)
    * refactor: refactor reissue api
    
    * refactor: refactor email verification
    
    * refactor : rename verifyEmail -> verifyDuplicateEmail
    
    * refactor: refactor email send-code api
    
    * refactor: rename to sendAuthenticationCode
    
    * refactor: refactor code verification api
    
    * feat: add logging
    
    ---------
    
    Co-authored-by: Yuje-Lee <65337423+dbwp031@users.noreply.github.com>

--- api/src/main/java/com/sponus/sponusbe/auth/controller/AuthController.java
+++ api/src/main/java/com/sponus/sponusbe/auth/controller/AuthController.java
@@ -2,70 +2,47 @@ package com.sponus.sponusbe.auth.controller;
 
 import static com.sponus.sponusbe.global.enums.ApiPath.*;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sponus.coredomain.domain.common.ApiResponse;
-import com.sponus.coredomain.domain.organization.repository.OrganizationRepository;
-import com.sponus.coreinfraemail.EmailUtil;
-import com.sponus.coreinfrasecurity.jwt.dto.JwtPair;
-import com.sponus.coreinfrasecurity.jwt.util.JwtUtil;
+import com.sponus.sponusbe.auth.dto.EmailVerificationResponse;
+import com.sponus.sponusbe.auth.dto.ReissueResponse;
+import com.sponus.sponusbe.auth.dto.SendAuthenticationCodeResponse;
+import com.sponus.sponusbe.auth.dto.VerifyAuthenticationCodeResponse;
+import com.sponus.sponusbe.auth.service.AuthService;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
 @RequiredArgsConstructor
 @RequestMapping(AUTH_URI)
 @RestController
 public class AuthController {
 
-	private final JwtUtil jwtUtil;
-	private final OrganizationRepository organizationRepository;
-	private final EmailUtil emailUtil;
+	private final AuthService authService;
 
 	@GetMapping("/reissue")
-	public ApiResponse<JwtPair> reissueToken(@RequestHeader("RefreshToken") String refreshToken) {
-		return ApiResponse.onSuccess(jwtUtil.reissueToken(refreshToken));
+	public ApiResponse<ReissueResponse> reissueToken(@RequestHeader("RefreshToken") String refreshToken) {
+		return ApiResponse.onSuccess(authService.reissueToken(refreshToken));
 	}
 
 	@GetMapping("/verify-email")
-	public ApiResponse<Map<String, String>> verifyEmail(@RequestHeader("email") String email) {
-		String emailExists =
-			Boolean.TRUE.equals(organizationRepository.checkDuplicateEmail(email)) ? "EXIST" : "NOT_EXIST";
-
-		Map<String, String> response = new HashMap<>();
-		response.put("email", email);
-		response.put("exist", emailExists);
-
-		return ApiResponse.onSuccess(response);
+	public ApiResponse<EmailVerificationResponse> verifyDuplicateEmail(@RequestHeader("email") String email) {
+		return ApiResponse.onSuccess(authService.verifyDuplicateEmail(email));
 	}
 
 	@GetMapping("/send-code")
-	public ApiResponse<Map<String, String>> sendAuthenticationEmail(@RequestHeader("email") String email) throws
+	public ApiResponse<SendAuthenticationCodeResponse> sendAuthenticationCode(@RequestHeader("email") String email) throws
 		Exception {
-		Map<String, String> response = new HashMap<>();
-		response.put("email", email);
-		response.put("code", emailUtil.sendEmail(email));
-
-		return ApiResponse.onSuccess(response);
+		return ApiResponse.onSuccess(authService.sendAuthenticationCode(email));
 	}
 
 	@GetMapping("/verify-code")
-	public ApiResponse<Map<String, String>> verifyAuthenticationCode(
+	public ApiResponse<VerifyAuthenticationCodeResponse> verifyAuthenticationCode(
 		@RequestHeader("email") String email,
 		@RequestHeader("code") String code) {
-		Map<String, String> response = new HashMap<>();
-
-		response.put("email", email);
-		response.put("flag", emailUtil.verifyCode(email, code));
-
-		return ApiResponse.onSuccess(response);
+		return ApiResponse.onSuccess(authService.verifyAuthenticationCode(email, code));
 	}
-
 }
