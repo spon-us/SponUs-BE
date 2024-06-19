ec945ea17a05dea2749f6211d68fcc76a032d20c
seheonnn <ho78901@naver.com>
2024-06-20 00:52:47 +0900

    fix: 헬스체크 & ddl-auto 수정

--- api/src/main/java/com/sponus/sponusbe/auth/controller/HealthCheck.java
+++ api/src/main/java/com/sponus/sponusbe/auth/controller/HealthCheck.java
@@ -3,9 +3,6 @@ package com.sponus.sponusbe.auth.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import lombok.RequiredArgsConstructor;
-
-@RequiredArgsConstructor
 @RestController
 public class HealthCheck {
 
